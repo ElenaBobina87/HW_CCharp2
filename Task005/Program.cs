f9bfbe17a1a8b7bdfcbf2baf28824ec505297087
elena bobina <putnyk19@mail.ru>
2023-09-08 11:03:06 +0500

    Task5.1

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 System.Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num>99 && num<1000)
+if (num > 99 && num < 1000)
 {
-    int result=num/10%10;
+    int result = num / 10 % 10;
     System.Console.WriteLine(result);
 }
 else
